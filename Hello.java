23968d02ee87ba8d801e5d297f986773f646b31a
Anne-Mari Kasemetsa <annemari.kasemetsa3@gmail.com>
2023-02-26 15:01:55 +0200

    change #2

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ package Github;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("friend was here");
+        System.out.println("friend was still here");
     }
 }
